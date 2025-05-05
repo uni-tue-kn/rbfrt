71059081281ef98bd7bbe0b4a43d97617348b4c6
etienneZink <etienneZink@users.noreply.github.com>
2025-05-05 13:58:45 +0000

    deploy: 28650248edccb2dba4d15c69043a8dfc8bcaa9e3

--- trait.impl/core/default/trait.Default.js
+++ trait.impl/core/default/trait.Default.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["rbfrt",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.1/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"rbfrt/util/struct.PrettyPrinter.html\" title=\"struct rbfrt::util::PrettyPrinter\">PrettyPrinter</a>"]]]]);
+    var implementors = Object.fromEntries([["rbfrt",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.86.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"rbfrt/util/struct.PrettyPrinter.html\" title=\"struct rbfrt::util::PrettyPrinter\">PrettyPrinter</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
