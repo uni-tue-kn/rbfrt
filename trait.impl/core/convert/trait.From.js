583e36676391549ee881e00529c2805654565f9b
n1tr0-5urf3r <n1tr0-5urf3r@users.noreply.github.com>
2025-09-22 10:10:04 +0000

    deploy: d733fa7b9dc162c4c1b404da9dad56669ad970b4

--- trait.impl/core/convert/trait.From.js
+++ trait.impl/core/convert/trait.From.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["rbfrt",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.89.0/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;Status&gt; for <a class=\"enum\" href=\"rbfrt/error/enum.RBFRTError.html\" title=\"enum rbfrt::error::RBFRTError\">RBFRTError</a>"]]]]);
+    var implementors = Object.fromEntries([["rbfrt",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.90.0/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;Status&gt; for <a class=\"enum\" href=\"rbfrt/error/enum.RBFRTError.html\" title=\"enum rbfrt::error::RBFRTError\">RBFRTError</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
---
+++ trait.impl/serde_core/ser/trait.Serialize.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["rbfrt",[["impl <a class=\"trait\" href=\"https://docs.rs/serde_core/1.0.226/serde_core/ser/trait.Serialize.html\" title=\"trait serde_core::ser::Serialize\">Serialize</a> for <a class=\"enum\" href=\"rbfrt/util/enum.AutoNegotiation.html\" title=\"enum rbfrt::util::AutoNegotiation\">AutoNegotiation</a>"],["impl <a class=\"trait\" href=\"https://docs.rs/serde_core/1.0.226/serde_core/ser/trait.Serialize.html\" title=\"trait serde_core::ser::Serialize\">Serialize</a> for <a class=\"enum\" href=\"rbfrt/util/enum.FEC.html\" title=\"enum rbfrt::util::FEC\">FEC</a>"],["impl <a class=\"trait\" href=\"https://docs.rs/serde_core/1.0.226/serde_core/ser/trait.Serialize.html\" title=\"trait serde_core::ser::Serialize\">Serialize</a> for <a class=\"enum\" href=\"rbfrt/util/enum.Loopback.html\" title=\"enum rbfrt::util::Loopback\">Loopback</a>"],["impl <a class=\"trait\" href=\"https://docs.rs/serde_core/1.0.226/serde_core/ser/trait.Serialize.html\" title=\"trait serde_core::ser::Serialize\">Serialize</a> for <a class=\"enum\" href=\"rbfrt/util/enum.Speed.html\" title=\"enum rbfrt::util::Speed\">Speed</a>"],["impl <a class=\"trait\" href=\"https://docs.rs/serde_core/1.0.226/serde_core/ser/trait.Serialize.html\" title=\"trait serde_core::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"rbfrt/util/struct.Digest.html\" title=\"struct rbfrt::util::Digest\">Digest</a>"],["impl <a class=\"trait\" href=\"https://docs.rs/serde_core/1.0.226/serde_core/ser/trait.Serialize.html\" title=\"trait serde_core::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"rbfrt/util/struct.Port.html\" title=\"struct rbfrt::util::Port\">Port</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[1658]}
