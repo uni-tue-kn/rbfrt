efa2e9990a1ce7afc1453fea689d6de3cdf9e824
n1tr0-5urf3r <n1tr0-5urf3r@users.noreply.github.com>
2026-04-22 14:14:48 +0000

    deploy: e56a434c325f5f9a1dda6e669e4f083d11d238e4

--- trait.impl/core/convert/trait.From.js
+++ trait.impl/core/convert/trait.From.js
@@ -1,5 +1,5 @@
 (function() {
-    const implementors = Object.fromEntries([["rbfrt",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.0/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;Status&gt; for <a class=\"enum\" href=\"rbfrt/error/enum.RBFRTError.html\" title=\"enum rbfrt::error::RBFRTError\">RBFRTError</a>",0]]]]);
+    const implementors = Object.fromEntries([["rbfrt",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.95.0/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;Status&gt; for <a class=\"enum\" href=\"rbfrt/error/enum.RBFRTError.html\" title=\"enum rbfrt::error::RBFRTError\">RBFRTError</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
--- trait.impl/core/default/trait.Default.js
+++ trait.impl/core/default/trait.Default.js
@@ -1,5 +1,5 @@
 (function() {
-    const implementors = Object.fromEntries([["rbfrt",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"rbfrt/util/struct.PrettyPrinter.html\" title=\"struct rbfrt::util::PrettyPrinter\">PrettyPrinter</a>",0]]]]);
+    const implementors = Object.fromEntries([["rbfrt",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.95.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"rbfrt/util/struct.PrettyPrinter.html\" title=\"struct rbfrt::util::PrettyPrinter\">PrettyPrinter</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
